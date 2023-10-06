225fe66d8a41e993b4d72348293029a0ff1fa73b
Pistolmannen <erikgustafsson03@gmail.com>
2023-10-06 17:17:13 +0200

    end of day

--- c++/Enarmad_Bandit/Enarmad_Bandit.cpp
+++ c++/Enarmad_Bandit/Enarmad_Bandit.cpp
@@ -4,11 +4,40 @@
 #include <Windows.h>    //används för att kunna sätta output konsollen till UTF-8
 using namespace std;
 
+
+string blank = " ";
+
+void language_set(int language){
+
+    if (language == 1){
+        
+    }
+
+}
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     srand(time(0));
 
-    cout<< "häj";
+    int language;
+
+    while(true)
+    {
+        cout<< blank << endl;
+        cout<< "please chose a language" << endl;
+        cout<< "1 for english, 2 för svenska" << endl;
+        cin>> language; 
+
+        if (language == 1 || language == 2){
+            break;
+        } 
+        else{
+            cout<< "please try again | snälla försök igen" << endl;
+        }
+    }
+    
+
+    
     return 0;
 }
