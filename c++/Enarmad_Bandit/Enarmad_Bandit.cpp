7b97805bfffcece32f538d4a28190adde0bd482a
Pistolmannen <erikgustafsson03@gmail.com>
2023-10-09 16:13:17 +0200

    rules and deposit done

--- c++/Enarmad_Bandit/Enarmad_Bandit.cpp
+++ c++/Enarmad_Bandit/Enarmad_Bandit.cpp
@@ -7,14 +7,40 @@ using namespace std;
 
 string blank = " ";
 string welcome;
+string invalid_selection;
+string question_rules; 
+string rules_1;
+string rules_2;
+string rules_3;
+string rules_4;
+string rules_5;
+string rules_6;
+string question_deposit;
 
 void language_set(int language){
 
     if (language == 1){
+        invalid_selection = "Not a valid argument, please try again";
         welcome = "Hello and welcome to this slot machine";
+        question_rules = "Do you want to see the rules? (1 for yes, 2 for no)";
+        rules_1 = "The slot machine works like this";
+        rules_2 = "The player start by deciding how much money they want to deposit.";
+        rules_3 = "Then the player gets to deciding how much to bet on a round. (100, 300 or 500 kr)";
+        rules_4 = "The machine will then roll a random pattern with three symbols on a three by three grid.";
+        rules_5 = "If there is at least one row of three symbols either horizontally, vertically or diagonally then the player wins.";
+        rules_6 = "One row = two times bet, Three rows = three times bet, Five rows = five times bet, Full board = ten times bet.";
+        question_deposit = "please put in how much you wish to deposit in to the game (minimum of 100 kr)";
     }
     else if (language == 2){
+        invalid_selection = "Inte ett giltigt argument, snälla försök igen";
         welcome = "Hej och välkommen till denna enarmade bandit";
+        question_rules = "Vill du see reglerna? (1 för ja, 2 för nej)";
+        rules_1 = "Dena enarmade bandit fungerar så här";
+        rules_2 = "Spelaren börjar med att bestämma hur mycket pengar de vill sätta in.";
+        rules_3 = "Sedan får spelaren bestämma hur mycket den ska satsa på en runda. (100, 300 eller 500 kr)";
+        rules_4 = "Maskinen kommer sedan att rulla ett slumpmässigt mönster med tre symboler på ett tre gånger tre rutnät.";
+        rules_5 = "Om det finns minst en rad med tre symboler antingen horisontellt, vertikalt eller diagonalt så vinner spelaren.";
+        rules_6 = "En rad = två gånger insats, Tre rader = tre gånger insats, Fem rader = fem gånger insats, Fullt bord = tio gånger insats.";
     }
     else{
         cout<< "how did this happen?????" << endl;
@@ -28,6 +54,11 @@ int main()
     srand(time(0));
 
     int language;
+    int rules_choice;
+    int deposit_choice;
+
+    int total_money;
+    int total_money_change;
 
     while(true)
     {
@@ -48,6 +79,51 @@ int main()
 
     cout<< welcome << endl;
 
-    
+    while (true){
+        cout<< blank << endl;
+        cout<< question_rules << endl;
+
+        cin >> rules_choice;
+
+        if (rules_choice == 1){
+            cout<< blank << endl;
+            cout<< rules_1 << endl;
+            cout<< rules_2 << endl;
+            cout<< rules_3 << endl;
+            cout<< rules_4 << endl;
+            cout<< rules_5 << endl;
+            cout<< rules_6 << endl;
+
+            _sleep(5000);
+            break;
+        }
+        else if(rules_choice == 2){
+            break;
+        }
+        else{
+            cout<< invalid_selection << endl;
+        }
+    }
+
+    while (true){
+        cout<< blank << endl;
+        cout<< question_deposit << endl;
+
+        cin >> deposit_choice;
+
+        if (deposit_choice < 100){
+            cout<< invalid_selection << endl;
+        }
+        else{
+            total_money = deposit_choice;
+            break;
+        }
+    }
+
+
+    cout<< blank << endl;
+    cout<< total_money << endl;
+    cout<< "Detta är slutet" << endl;
+
     return 0;
 }
